f05f5f8607a2feb3852eda6ed2d7195847821ed3
hanshalbhatiya <91733240+hanshalbhatiya@users.noreply.github.com>
2021-12-14 17:16:51 +0530

    nested file base routing
    learned nested file base routing...

---
+++ pages/blog/blog1.js
@@ -0,0 +1,7 @@
+const blog1 = () => {
+    return (
+        <h1>blog1</h1>
+    )
+}
+
+export default blog1
---
+++ pages/blog/blog2.js
@@ -0,0 +1,7 @@
+const blog2 = () => {
+    return (
+        <h1>blog2</h1>
+    )
+}
+
+export default blog2
